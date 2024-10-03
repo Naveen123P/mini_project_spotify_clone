1be031b6f2cef9c9d0ad6202aee8ba3d8d8f8ce9
Naveen <pothurajunaveen963@gmail.com>
2024-10-03 07:31:22 +0000

    s

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -4,8 +4,10 @@ import Cookies from 'js-cookie'
 import Popup from 'reactjs-popup'
 import {IoReorderThree} from 'react-icons/io5'
 import {IoIosClose} from 'react-icons/io'
+import PlayListsDetails from '../PlayListsDetails'
 import LoaderRoute from '../LoaderRoute'
-import FailureRoute from '../FailureRoute'
+import FailureView from '../FailureView'
+import SideHeader from '../SideHeader'
 import './index.css'
 
 const apiStatusConstants = {
@@ -27,14 +29,78 @@ class Home extends Component {
 
   componentDidMount() {
     this.getEditorsPicks()
-    // this.getGenresMoods()
+    this.getGenresMoods()
     // this.getNewReleases()
   }
 
-  getEditorsPicks = () => {
+  formattedData = data => ({
+    id: data.id,
+    name: data.name,
+    image: data.images[0].url,
+  })
+
+  categoriesFormattedData = data => ({
+    id: data.id,
+    name: data.name,
+    image: data.icons[0].url,
+  })
+
+  getEditorsPicks = async () => {
     this.setState({
       editorsPicksApiStatus: apiStatusConstants.inProgress,
     })
+    const token = Cookies.get('jwt_token')
+    const url = 'https://apis2.ccbp.in/spotify-clone/featured-playlists'
+    const options = {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+      method: 'GET',
+    }
+    const response = await fetch(url, options)
+    if (response.ok) {
+      const data = await response.json()
+      const updatedData = data.playlists.items.map(each =>
+        this.formattedData(each),
+      )
+
+      console.log(updatedData)
+      this.setState({
+        editorsPicksApiStatus: apiStatusConstants.failure,
+        editorsPicks: updatedData,
+      })
+    } else {
+      this.setState({editorsPicksApiStatus: apiStatusConstants.failure})
+    }
+  }
+
+  getGenresMoods = async () => {
+    this.setState({
+      genresMoodsApiStatus: apiStatusConstants.inProgress,
+    })
+    const token = Cookies.get('jwt_token')
+    const url = 'https://apis2.ccbp.in/spotify-clone/categories'
+    const options = {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+      method: 'GET',
+    }
+    const response = await fetch(url, options)
+    if (response.ok) {
+      const data = await response.json()
+      const updatedData = data.categories.items.map(each =>
+        this.categoriesFormattedData(each),
+      )
+
+      console.log(updatedData)
+      this.setState({
+        genresMoodsApiStatus: apiStatusConstants.failure,
+        genresMoods: updatedData,
+      })
+    } else {
+      this.setState({genresMoodsApiStatus: apiStatusConstants.failure})
+    }
   }
 
   onClickLogout = () => {
@@ -91,46 +157,87 @@ class Home extends Component {
     </div>
   )
 
-  getAllApiStatus = apiStatus => {
-    switch (apiStatus) {
+  renderEditorsPicksSuccessView = () => {
+    const {editorsPicks} = this.state
+    return (
+      <>
+        <ul className="playlist-ul">
+          {editorsPicks.map(each => (
+            <PlayListsDetails key={each.id} details={each} />
+          ))}
+        </ul>
+      </>
+    )
+  }
+
+  renderGenresMoodsSuccessView = () => {
+    const {genresMoods} = this.state
+    return (
+      <>
+        <ul className="playlist-ul">
+          {genresMoods.map(each => (
+            <PlayListsDetails key={each.id} details={each} />
+          ))}
+        </ul>
+      </>
+    )
+  }
+
+  editorsPicksRetry = () => {
+    this.getEditorsPicks()
+  }
+
+  genresMoodsRetry = () => {
+    this.getGenresMoods()
+  }
+
+  getAllEditorsPicksApiStatus = () => {
+    const {editorsPicksApiStatus} = this.state
+    switch (editorsPicksApiStatus) {
       case apiStatusConstants.inProgress:
         return <LoaderRoute />
-      //   case apiStatusConstants.success:
-      //     return successView()
-      //   case apiStatusConstants.failure:
-      //     return <FailureRoute retry={retry} />
+      case apiStatusConstants.success:
+        return this.renderEditorsPicksSuccessView()
+      case apiStatusConstants.failure:
+        return <FailureView retry={this.editorsPicksRetry} />
       default:
         return null
     }
   }
 
-  renderEditorsPicksSuccessView = () => {
-    const {editorsPicks} = this.editorsPicks
-    return <h1>success</h1>
-  }
-
-  getAllEditorsPicks = () => {
-    const {editorsPicksApiStatus} = this.state
-    const retry = () => {
-      this.getEditorsPicks()
+  getAllGenresMoodsApiStatus = () => {
+    const {genresMoodsApiStatus} = this.state
+    switch (genresMoodsApiStatus) {
+      case apiStatusConstants.inProgress:
+        return <LoaderRoute />
+      case apiStatusConstants.success:
+        return this.renderGenresMoodsSuccessView()
+      case apiStatusConstants.failure:
+        return <FailureView retry={this.genresMoodsRetry} />
+      default:
+        return null
     }
-
-    this.getAllApiStatus(editorsPicksApiStatus)
-    //   ,
-    //   this.renderEditorsPicksSuccessView(),
-    //   retry,
-    // )
   }
 
   render() {
     const {editorsPicks, genresMoods, newReleases} = this.state
     return (
       <>
-        <>{this.renderHomeHeader()}</>
         <div className="mobile-view">
+          <>{this.renderHomeHeader()}</>
+          <div className="home-bg">{this.getAllEditorsPicksApiStatus()}</div>
+        </div>
+        <div className="desktop-view">
+          <SideHeader />
           <div className="home-bg">
-            <h1>Home</h1>
-            {this.getAllEditorsPicks()}
+            <div className="playlist-container">
+              <h1 className="playlist-title">Editor's Picks</h1>
+              {this.getAllEditorsPicksApiStatus()}
+            </div>
+            <div className="playlist-container">
+              <h1 className="playlist-title">Genres & Moods</h1>
+              {this.getAllGenresMoodsApiStatus()}
+            </div>
           </div>
         </div>
       </>
